1735fc1dd67b8ae6d3157baedeae9f639070ec86
Cleidson Estevam da Silva <cleidson.silva@reval.corp>
2022-03-22 14:36:43 -0300

    MinimalAPI V2

--- DemoMinimalAPI/DemoMinimalAPI/Program.cs
+++ DemoMinimalAPI/DemoMinimalAPI/Program.cs
@@ -1,7 +1,17 @@
+using DemoMinimalAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region "Mapeando ConnectionString"
+builder.Services.AddDbContext<MinimalContextDb>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+#endregion
+
 
 var app = builder.Build();
 
